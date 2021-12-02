a00c83a9c76325993395e1b657f8a2a3b65d9918
Amal Kaduwela <akaduwel@ucsd.edu>
2021-12-02 12:50:39 -0800

    Edits to make ddb work

--- components/customNavbar.js
+++ components/customNavbar.js
@@ -10,7 +10,6 @@
         const template = document.createElement('template');
 
         template.innerHTML = `
-        <script src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
           <style>
@@ -41,41 +40,44 @@
             }   
           </style>
           <!-- Navbar Start -->
-            <nav class="navbar navbar-expand-lg navbar-dark">
-            <a class="navbar-brand" href="/index.html">amal_kaduwela</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
-                <form class="d-flex">
-                <ul class="navbar-nav d-flex">
-                    <li class="nav-item">
-                    <a class="nav-link" href="/index.html">About Me</a>
-                    </li>
-                    <li class="nav-item">
-                    <a class="nav-link" href="/pages/links.html">Links</a>
-                    </li>
-                    <li class="nav-item">
-                    <a class="nav-link" href="/pages/critique.html">Critique</a>
-                    </li>
-                    <li class="nav-item">
-                    <a class="nav-link" href="/pages/NWElement.html">Non-Written Element</a>
-                    </li>
-                    <li class="nav-item">
-                    <a class="nav-link" href="/pages/resume.html">Resume</a>
-                    </li>
-                    <li class="nav-item">
-                    <a class="nav-link" href="/pages/music.html">Music</a>
-                    </li>
-                </ul>
-                </form>
-            </div>
-            </nav>
+
+
+          <nav class="navbar navbar-expand- navbar-dark">
+          <a class="navbar-brand" href="/index.html">amal_kaduwela</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+              <form class="d-flex">
+              <ul class="navbar-nav d-flex">
+                  <li class="nav-item">
+                  <a class="nav-link" href="/index.html">About Me</a>
+                  </li>
+                  <li class="nav-item">
+                  <a class="nav-link" href="/pages/links.html">Links</a>
+                  </li>
+                  <li class="nav-item">
+                  <a class="nav-link" href="/pages/critique.html">Critique</a>
+                  </li>
+                  <li class="nav-item">
+                  <a class="nav-link" href="/pages/NWElement.html">Non-Written Element</a>
+                  </li>
+                  <li class="nav-item">
+                  <a class="nav-link" href="/pages/resume.html">Resume</a>
+                  </li>
+                  <li class="nav-item">
+                  <a class="nav-link" href="/pages/music.html">Music</a>
+                  </li>
+              </ul>
+              </form>
+          </div>
+          </nav>
           <!-- Navbar End -->
+
           `;
 
-        this.attachShadow({ mode: 'open' });
-        this.shadowRoot.appendChild(template.content.cloneNode(true));
+        this.attach({ mode: 'open' });
+        this.appendChild(template.content.cloneNode(true));
 
         let title = document.querySelector(".big-heading");
         let navLinks = this.shadowRoot.querySelectorAll(".nav-link");
